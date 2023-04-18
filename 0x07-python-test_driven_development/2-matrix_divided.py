cf1041faaf0590de26d411b2f2ee2b6f777155a4
Trisraki <trisraquel1@gmail.com>
2023-04-17 18:12:19 -0700

    2-matrix_divided.py, tests/2-matrix_divided.txt

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -26,4 +26,4 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
     new_matrix = list(map(lambda row:
                           list(map(lambda x: round(x/div, 2), row)), matrix))
-    return new_matrix
+    return  new_matrix
