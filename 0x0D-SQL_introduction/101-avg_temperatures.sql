067b479240cdc555ae46c658a781b700c5501b89
Trisraki <trisraquel1@gmail.com>
2023-05-16 19:59:45 -0700

    fix

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,4 @@
+-- Import in hbtn_0c and display temp by city and ordered by temp
+-- A script that displays the average temperature (Fahrenheit) by
+-- city ordered by temperature (descending).
+SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- This script contains some SQL commands thst prints the
+-- full description of the table 'first_table' from the
+-- database 'hbtn_0c_0' of your MySQL server.
+SHOW CREATE TABLE first_table
