b0efacc66e32a426d393d86e371b1afdddab9eb2
Trisraki <trisraquel1@gmail.com>
2023-06-14 11:53:47 -0700

    fix

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
+exports.add = (a, b) => {
+  return a + b;
 };
-console.log(myObject);
-myObject.value = 89;
-console.log(myObject);
