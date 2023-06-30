c1e8dbd3c30f9f853f14cfdb0f5ae1f978dd9819
Trisraki <trisraquel1@gmail.com>
2023-06-30 14:48:09 -0700

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-fetch https://intranet.hbtn.io/status; display response
+Script that fetches https://intranet.hbtn.io/status
 """
 import requests
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+if __name__ == '__main__':
+    url = "https://intranet.hbtn.io/status"
+    r = requests.get(url)
+    text = r.text
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print(f"\t- type: {type(text)}")
+    print(f"\t- content: {text}")
